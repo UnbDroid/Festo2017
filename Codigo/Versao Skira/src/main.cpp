47d85125ac9cfe23996123bc668ec7e3e38d20d5
Leticia Porto <leticiahelenasp@hotmail.com>
2017-11-01 14:14:32 -0200

    versao skira

--- Codigo/Versao Skira/src/main.cpp
+++ Codigo/Versao Skira/src/main.cpp
@@ -58,7 +58,7 @@ int main( int argc, char **argv )
 
 	FabricaDeMapas fab;
 	Mapa mapa;
-	fab.criarMapa("Modulo1_001",mapa);
+	fab.criarMapa("mapaArenaTreino",mapa);
 	mapa.mostrar_mapa();
 	waitKey();
 	try{
